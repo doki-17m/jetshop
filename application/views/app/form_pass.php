9ac99e72b885f63fa3373444d57cf02ec7d8baf4
Tintapen <Oki@HPDQ1037-OKI.SAS1.local>
2020-11-09 13:38:28 +0700

    Add form change password

---
+++ application/views/app/form_pass.php
@@ -0,0 +1,25 @@
+<form class="form-horizontal" id="form_chgpass">
+	<div class="card-body">
+		<div class="form-group row">
+			<label for="chg_oldpass" class="col-sm-3 col-form-label">Old Password</label>
+			<div class="col-sm-8">
+				<input type="password" class="form-control" id="chg_oldpass" name="chg_oldpass" placeholder="Enter your old password">
+				<small id="error_chg_oldpass" class="form-text text-danger"></small>
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="chg_newpass" class="col-sm-3 col-form-label">New Password</label>
+			<div class="col-sm-8">
+				<input type="password" class="form-control" id="chg_newpass" name="chg_newpass" placeholder="Enter your new password">
+				<small id="error_chg_newpass" class="form-text text-danger"></small>
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="chg_confpass" class="col-sm-3 col-form-label">Confirmation Password</label>
+			<div class="col-sm-8">
+				<input type="password" class="form-control" id="chg_confpass" name="chg_confpass" placeholder="Enter confirmation password">
+				<small id="error_chg_confpass" class="form-text text-danger"></small>
+			</div>
+		</div>
+	</div>
+</form>
