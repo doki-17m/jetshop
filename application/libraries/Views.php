e9bc62c585636ac2c71322a9102198db9c68c4e0
Tintapen <Oki@HPDQ1037-OKI.SAS1.local>
2020-11-22 10:50:31 +0700

    Add view_expense public variable

--- application/libraries/Views.php
+++ application/libraries/Views.php
@@ -1,13 +1,14 @@
 <?php
 
-class Views {
+class Views
+{
 
 	public $OVERVIEW = 'overview'; //dashboard link
 
 	public $VIEW_LOGIN = 'app/v_login'; //view login
 
 	public $VIEW_DASHBOARD = 'app/v_dashboard'; //view category
-	
+
 	public $VIEW_PRODUCT = 'product/v_product'; //view product
 
 	public $VIEW_CATEGORY = 'category/v_category'; //view category
@@ -23,18 +24,20 @@ class Views {
 	public $VIEW_CUSTOMER = 'customer/v_customer'; //view customer
 
 	public $VIEW_DESTINATION = 'destination/v_destination'; //view destination
-	
+
 	public $VIEW_PROVINCE = 'destination/v_province'; //view province
 
 	public $VIEW_CITY = 'destination/v_city'; //view city
 
 	public $VIEW_COURIER = 'courier/v_courier'; //view courier
 
-	public $VIEW_ACCOUNT = 'account/v_account'; //view courier
+	public $VIEW_ACCOUNT = 'account/v_account'; //view account
 
 	public $VIEW_USER = 'user/v_user'; //view user
 
-	public $VIEW_POS = 'sales/v_pos'; //view user
+	public $VIEW_POS = 'sales/v_pos'; //view pos
 
 	public $VIEW_SETTING = 'setting/v_setting'; //view setting
+
+	public $VIEW_EXPENSE = 'expense/v_expense'; //view expense
 }
