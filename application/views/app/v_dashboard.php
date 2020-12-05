61a54be69561911e3c4ecb4c2dcb2d9dd0a97d1e
Tintapen <Oki@HPDQ1037-OKI.SAS1.local>
2020-12-05 10:15:07 +0700

    Nonaktif Box info

--- application/views/app/v_dashboard.php
+++ application/views/app/v_dashboard.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="col-lg-3 col-6">
 				<!-- small box -->
-				<div class="small-box bg-info">
+				<!-- <div class="small-box bg-info">
 					<div class="inner">
 						<h3>150</h3>
 
@@ -13,11 +13,11 @@
 						<i class="ion ion-bag"></i>
 					</div>
 					<a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-				</div>
+				</div> -->
 			</div>
 			<div class="col-lg-3 col-6">
 				<!-- small box -->
-				<div class="small-box bg-success">
+				<!-- <div class="small-box bg-success">
 					<div class="inner">
 						<h3>53<sup style="font-size: 20px">%</sup></h3>
 
@@ -27,7 +27,7 @@
 						<i class="ion ion-stats-bars"></i>
 					</div>
 					<a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-				</div>
+				</div> -->
 			</div>
 		</div>
 	</div>
