a62fa50124e4d4c177491a8d21bf3ce0a3a43029
Tintapen <Oki@HPDQ1037-OKI.SAS1.local>
2020-12-06 11:42:42 +0700

    Create view order

---
+++ application/views/sales/v_order.php
@@ -0,0 +1,30 @@
+<div class="row">
+	<div class="col-12">
+		<div class="card">
+			<div class="card-body">
+				<table id="tb_sales" class="table table-bordered table-hover table-pointer" style="width: 100%">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>#</th>
+							<th>Invoice No</th>
+							<th>Customer</th>
+							<th>Date</th>
+							<th>Address</th>
+							<th>Destination</th>
+							<th>Courier</th>
+							<th>Service</th>
+							<th>Total Product</th>
+							<th>Weight</th>
+							<th>Amount</th>
+							<th>Cashier</th>
+							<th>Member</th>
+							<th>Status</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
