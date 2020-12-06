cc70d88c6239738ff4417b194aac576b5b915a68
Tintapen <Oki@HPDQ1037-OKI.SAS1.local>
2020-12-06 11:40:50 +0700

    Change title print and new function ismember

--- application/helpers/action_helper.php
+++ application/helpers/action_helper.php
@@ -34,7 +34,7 @@ function listAction($id, $string)
 					</center>';
 	} else if ($string === 'P') {
 		$list = '<center>
-						<a class="btn" onclick="Print(' . "'" . $id . "'" . ')" title="Cart"><i class="fas fa-print text-primary"></i></a>
+						<a class="btn" onclick="Print(' . "'" . $id . "'" . ')" title="Print"><i class="fas fa-print text-primary"></i></a>
 					</center>';
 	} else if ($string === 'DQ') {
 		$list = '<center>
@@ -108,5 +108,14 @@ function docStatus($id, $string)
 		return '<center><span class="badge badge-danger">Voided</span></center>';
 	} else {
 		return '<center><a onclick="processDoc(' . "'" . $id . "'," . "'" . $string . "'" . ')" title="Drafted"><span class="badge badge-warning">Drafted</span></a></center>';
+	}
+}
+
+function isMember($string)
+{
+	if ($string == 'Y') {
+		return '<center><span class="badge badge-primary">Yes</span></center>';
+	} else {
+		return '<center><span class="badge badge-danger">No</span></center>';
 	}
 }
