1922995272595b627e775cb3c14494a6fe38d3b4
Tintapen <Oki@HPDQ1037-OKI.SAS1.local>
2020-12-11 15:58:56 +0700

    Remove class table-responsive on div, add class table-md on table return

---
+++ application/views/return/v_return.php
@@ -0,0 +1,27 @@
+<div class="row">
+	<div class="col-12">
+		<div class="card">
+			<div class="card-header">
+				<div class="float-left">
+					<button type="button" class="btn btn-block bg-gradient-primary btn-sm" id="new_return"><i class="fas fa-plus-circle"> New</i></button>
+				</div>
+			</div>
+			<div class="card-body">
+				<table id="tb_return" class="table table-bordered table-hover table-pointer table-md" style="width: 100%">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>#</th>
+							<th>Document No</th>
+							<th>Date</th>
+							<th>Line Product</th>
+							<th>Grand Total</th>
+							<th>Note</th>
+							<th>Status</th>
+						</tr>
+					</thead>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
