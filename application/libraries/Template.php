683b0b98bfe19a20bfbeb037f8049874ac4213ad
Tintapen <okiper12@gmail.com>
2021-04-11 10:59:23 +0700

    Update content session user

--- application/libraries/Template.php
+++ application/libraries/Template.php
@@ -1,20 +1,27 @@
 <?php
 
-class Template {
+class Template
+{
 
-  var $template_data = array();
+	var $template_data = array();
 
-  public function __construct()
-  {
-		$this->CI =& get_instance();
-  }
+	public function __construct()
+	{
+		$this->CI = &get_instance();
+		$this->CI->load->model('m_user');
+	}
 
-  function set($name, $value){
-    $this->template_data[$name] = $value;
-  }
+	function set($name, $value)
+	{
+		$this->template_data[$name] = $value;
+	}
 
-  function load($template = '', $view = '', $view_data = array(), $return = FALSE){
-    $this->set('contents', $this->CI->load->view($view, $view_data, TRUE));
-    return $this->CI->load->view($template, $this->template_data, $return);
-  }
+	function load($template = '', $view = '', $view_data = array(), $return = FALSE)
+	{
+		$user_id = $this->CI->session->userdata('user_id');
+		$view_data['username'] = $this->CI->m_user->detail($user_id)->row();
+
+		$this->set('contents', $this->CI->load->view($view, $view_data, TRUE));
+		return $this->CI->load->view($template, $this->template_data, $return);
+	}
 }
